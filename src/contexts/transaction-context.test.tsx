023f91fe9a44000af387e7a1754b2cdb44fa1587
denisos <deniskos@hotmail.com>
2022-05-27 10:11:21 -0700

    added context specific unit test with api spy

---
+++ src/contexts/transaction-context.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen, act, waitFor } from '@testing-library/react';
+
+import { TransactionProvider, useTransactions } from './transaction-context';
+
+import { NearTransaction } from '../types/types';
+import { data } from '../mocks/transactions';
+
+import * as API from '../api/api';
+
+
+describe('transaction-context', () => {
+  const MockChildComponent = () => {
+    const { transactions, loading, error } = useTransactions();
+
+    return (
+      <>
+        <h1>Child Loaded</h1>
+        {error && <h1>Child Error</h1>}
+      </>
+    );
+  };
+
+  test('should call api and render child on success', async () => {
+    // spy on the getTransactions fn and return mock data so we can check it was called
+    //  as expected
+    const apiSpy = jest.spyOn(API, 'getTransactions')
+      .mockImplementation(() => {
+        return Promise.resolve(data as NearTransaction[]);
+      });
+
+    render(
+      <TransactionProvider>
+        <MockChildComponent />
+      </TransactionProvider>
+    )
+
+    await waitFor(() => {
+      // success
+      expect(screen.getByText(/Child Loaded/i)).toBeInTheDocument();
+
+      // no error
+      const errorEl = screen.queryByText(/Child Error/i);
+      expect(errorEl).toBe(null);
+
+      expect(apiSpy).toHaveBeenCalledTimes(1);
+    });
+    
+    // if we did not have the waitFor above then could also use the following
+    //  where dataPromise is the promise returned by the spy
+    // await act(async () => {
+    //   await dataPromise
+    // })
+  });
+});
